4503e7216f0543201c75cb510057b052472ac4f3
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-11-15 12:24:01 +0100

    Add files via upload

---
+++ SEDC.NotesApp/Controllers/NotesController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.NotesApp.Dtos;
+using SEDC.NotesApp.Services.Interfaces;
+
+namespace SEDC.NotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NotesController(INoteService noteService) //DI
+        {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<NoteDto>> Get()
+        {
+            var notes = _noteService.GetAllNotes();
+            return Ok(notes);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<NoteDto> GetById(int id)
+        {
+            try
+            {
+                var note = _noteService.GetById(id);
+
+                return Ok(note);    
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occured: {ex.Message}"); 
+            }
+        }
+
+        [HttpPost("addNote")]
+        public IActionResult AddNote([FromBody] AddNoteDto addNoteDto)
+        {
+            try
+            {
+                _noteService.AddNote(addNoteDto);
+                return Ok("Note added successfully");
+
+            } 
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound($"Error: {ex.Message}");
+            }
+            catch(ArgumentException ex)
+            {
+                return BadRequest($"{ex.Message}"); 
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNote([FromBody] UpdateNoteDto updateNoteDto)
+        {
+            try
+            {
+                _noteService.UpdateNote(updateNoteDto);
+                return Ok("Note updated successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(int id)
+        {
+            _noteService.DeleteNote(id);
+            return Ok("Note deleted successfully");
+        }
+    }
+}
---
+++ SEDC.NotesApp/Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.NotesApp.Domain.Models;
+using SEDC.NotesApp.Services.Implementations;
+using SEDC.NotesApp.Services.Interfaces;
+
+namespace SEDC.NotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService) 
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers() 
+        {
+            var users = _userService.GetAll();
+            return Ok(users);   
+        }
+
+        [HttpGet("{userId}")]
+        public ActionResult GetUserById(int userId) 
+        {
+            var user = _userService.GetById(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser([FromBody] User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid user data");
+            }
+            _userService.Add(user);
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public ActionResult UpdateUser(int userId, [FromBody] User user) 
+        {
+            if(user == null || userId != user.Id)
+            {
+                return BadRequest("Invalid user data");
+            }
+            _userService.Update(user);
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteUser(int userId) 
+        {
+            _userService.Delete(userId);
+            return Ok("Data successfully deleted");
+        }
+    }
+}
