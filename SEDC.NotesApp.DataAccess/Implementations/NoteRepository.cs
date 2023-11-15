b925bbc78254ed41230d914335d25e68d0923ea5
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-11-15 12:24:50 +0100

    Add files via upload

---
+++ SEDC.NotesApp.DataAccess/Implementations/NoteRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEDC.NotesApp.Domain.Models;
+
+namespace SEDC.NotesApp.DataAccess.Implementations
+{
+    public class NoteRepository : IRepository<Note>
+    {
+        private NotesAppDbContext _notesAppDbContext;
+
+        public NoteRepository(NotesAppDbContext notesAppDbContext) //DI
+        {
+            _notesAppDbContext = notesAppDbContext;
+        }
+
+        public void Add(Note entity)
+        {
+            _notesAppDbContext.Notes.Add(entity);
+
+            _notesAppDbContext.SaveChanges();
+        }
+
+        public void Delete(Note entity)
+        {
+            _notesAppDbContext.Notes.Remove(entity);    
+
+            _notesAppDbContext.SaveChanges();
+        }
+
+        public List<Note> GetAll()
+        {
+            List<Note> notes = _notesAppDbContext.Notes.Include(n => n.User).ToList();
+
+            return notes;  
+        }
+
+        public Note GetById(int id)
+        {
+            Note note = _notesAppDbContext.Notes
+                .Include(n => n.User)
+                .FirstOrDefault(n => n.Id == id);
+
+            return note;
+        }
+
+        public void Update(Note entity)
+        {
+            _notesAppDbContext.Notes .Update(entity);   
+            _notesAppDbContext.SaveChanges();   
+        }
+    }
+}
---
+++ SEDC.NotesApp.DataAccess/Implementations/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SEDC.NotesApp.Domain.Models;
+
+namespace SEDC.NotesApp.DataAccess.Implementations
+{
+    public class UserRepository : IRepository<User>
+    {
+        private NotesAppDbContext _notesAppDbContext;
+
+        public UserRepository(NotesAppDbContext notesAppDbContext)
+        {
+            _notesAppDbContext = notesAppDbContext;
+        }
+
+        public void Add(User entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _notesAppDbContext.Users.Add(entity);   
+            _notesAppDbContext.SaveChanges();   
+        }
+
+        public void Delete(User entity)
+        {
+            var user = _notesAppDbContext.Users.Find(entity);
+            if(user != null)
+            {
+                _notesAppDbContext.Users.Remove(user);
+                _notesAppDbContext.SaveChanges();
+            }
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            return _notesAppDbContext.Users
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(User entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _notesAppDbContext.Update(entity);
+            _notesAppDbContext.SaveChanges();
+        }
+    }
+}
